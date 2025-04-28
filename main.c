3bcd241bd89a3ff5c209dfee991a383e96099caf
Boris Gazur <barlukh@gmail.com>
2025-04-28 17:07:56 +0300

    Polish bee animation code

--- main.c
+++ main.c
@@ -6,11 +6,16 @@ int main(void)
 
 	const int screenWidth = 2560;
 	const int screenHeight = 1440;
-	const char *screenTitle = "Defenders of the Hive";
+	const char *screenTitle = "Defenders of the Hive!";
 
 	InitWindow(screenWidth, screenHeight, screenTitle);
+	// SetWindowState(FLAG_FULLSCREEN_MODE);
 
 	SetTargetFPS(60);
+	
+	int currentFrame = 0;
+	float frameTime = 0.2f;
+	float timeElapsed = 0.0f;
 
 	Texture2D background = LoadTexture("graphics/background.png");
 	Texture2D hive = LoadTexture("graphics/hive.png");
@@ -20,28 +25,22 @@ int main(void)
         LoadTexture("graphics/bee3.png"),
         LoadTexture("graphics/bee4.png")
 	};
-	
-	int currentFrame = 0;
-    float frameTime = 0.2f;
-    float timeElapsed = 0.0f;
 
 	while (!WindowShouldClose())
 	{
-		// Update animation frame
         timeElapsed += GetFrameTime();
         if (timeElapsed >= frameTime) {
-            currentFrame = (currentFrame + 1) % 4; // Loop back to the first image
+            currentFrame = (currentFrame + 1) % 4;
             timeElapsed = 0.0f;
         }
 
 		BeginDrawing();
 			DrawTexture(background, 0, 0, WHITE);
 			DrawTexture(hive, 80, 464, WHITE);
-			DrawTexture(bee[currentFrame], screenWidth/2 - bee[currentFrame].width/2,
-				screenHeight/2 - bee[currentFrame].height/2, WHITE);
-		
+			DrawTexture(bee[currentFrame], 1000, 600, WHITE);
 		EndDrawing();
 	}
+	
 	UnloadTexture(background);
 	UnloadTexture(hive);
 	for (int i = 0; i < 4; i++) {
